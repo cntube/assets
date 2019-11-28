0a5fcd51d575ac1b7de9ca1d2337895c21c66cac
cntube <46072715+cntube@users.noreply.github.com>
2019-11-28 18:24:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-var domains=['y.route.tuck.tw','y.route.linkin.tw','y.route.ns01.biz','y.route.ns02.biz','y.route.ygto.com','y.route.xn--cnqxyh92cdj7al4gr4w.hk','y.route.netlord.de','y.route.xxxxx.tw','y.route.biz.tm'];
+var domains = array(
+  'y.route.i-taiwan.tv',
+  'y.route.tuck.tw',
+  'y.route.linkin.tw',
+  'y.route.xxxxx.tw',
+  'y.route.biz.tm',
+  'y.route.netlord.de',
+  'y.route.lovethosetrains.com',
+  'y.route.ns01.biz',
+  'y.route.ns02.biz',
+  'y.route.ygto.com',
+);
